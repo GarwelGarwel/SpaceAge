781e8a59d5654157e71220c567828c02df05c459
GarwelGarwel <okozlovsky@gmail.com>
2017-10-25 10:16:17 +0300

    Version update to 0.2.4

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.2.3")]
-[assembly: AssemblyFileVersion("0.2.3")]
+[assembly: AssemblyVersion("0.2.4")]
+[assembly: AssemblyFileVersion("0.2.4")]
 [assembly: NeutralResourcesLanguage("en")]
 
