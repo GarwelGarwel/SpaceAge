783ea01930e2410e6d5402eb8075e95b02b13750
GarwelGarwel <okozlovsky@gmail.com>
2018-10-17 20:06:52 +0200

    Updated to v1.0.1

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Space Age")]
-[assembly: AssemblyDescription("Space Age mod for Kerbal Space Program records and displays chronicles of important game events.")]
+[assembly: AssemblyDescription("Space Age mod displays chronicles of important game events and achievements")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
 [assembly: NeutralResourcesLanguage("en")]
 
