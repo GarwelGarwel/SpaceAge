c660ead29df896c14d509fad0fbe9b75a6ef0eec
NuttySquabble <nutty.squabble@protonmail.com>
2020-05-17 17:24:37 +0300

    v1.2 update

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.2.0.1")]
-[assembly: AssemblyFileVersion("1.2.0.1")]
+[assembly: AssemblyVersion("1.2.0.3")]
+[assembly: AssemblyFileVersion("1.2.0.3")]
 [assembly: NeutralResourcesLanguage("en")]
 
